eb9527aef64818370b644e6653183b7419381e97
Viet Dao <mm16vd@leeds.ac.uk>
2018-10-22 13:50:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# tutorial
+
+Tutorial Files and tests
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Container(
+            child: Text("Hello World App Bar"),
+          ),
+        ),
+      )
+    );
+  }
+}
